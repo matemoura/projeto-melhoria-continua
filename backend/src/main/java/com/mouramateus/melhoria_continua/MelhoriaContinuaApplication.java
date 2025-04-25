b3ef14296dce5cceb7e01c41031b30634a46b610
Mateus Moura <mateus51moura@gmail.com>
2025-04-25 00:42:15 -0300

    init: configuração inicial das pastas backend e frontend

---
+++ backend/src/main/java/com/mouramateus/melhoria_continua/MelhoriaContinuaApplication.java
@@ -0,0 +1,13 @@
+package com.mouramateus.melhoria_continua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelhoriaContinuaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelhoriaContinuaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mouramateus/melhoria_continua/MelhoriaContinuaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mouramateus.melhoria_continua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelhoriaContinuaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
