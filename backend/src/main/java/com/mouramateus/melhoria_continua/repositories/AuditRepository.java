517c4935a5f658a6d3b59ebf9278d66710948115
Mateus Moura <mateus51moura@gmail.com>
2025-06-21 13:20:09 -0300

    feat(ranking): implement backend endpoints for total and latest 5S audit rankings

--- backend/src/main/java/com/mouramateus/melhoria_continua/repositories/AuditRepository.java
+++ backend/src/main/java/com/mouramateus/melhoria_continua/repositories/AuditRepository.java
@@ -4,6 +4,10 @@ import com.mouramateus.melhoria_continua.entities.Audit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AuditRepository extends JpaRepository<Audit, Long> {
+
+    Optional<Audit> findTopByOrderByAuditDateTimeDesc();
 }
