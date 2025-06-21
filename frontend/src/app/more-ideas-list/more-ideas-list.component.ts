74a7a61b971a701cc5b8be267fe96c81c22b0848
Mateus Moura <mateus51moura@gmail.com>
2025-06-20 22:46:19 -0300

    feat(frontend): create listing of existing ideas

--- frontend/src/app/more-ideas-list/more-ideas-list.component.ts
+++ frontend/src/app/more-ideas-list/more-ideas-list.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
+import { HttpClient } from '@angular/common/http'; 
+
+interface MoreIdea {
+  id: number;
+  nomeUsuario: string;
+  descricaoProblema: string;
+}
 
 @Component({
   selector: 'app-more-ideas-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './more-ideas-list.component.html',
-  styleUrl: './more-ideas-list.component.css'
+  styleUrls: ['./more-ideas-list.component.css']
 })
-export class MoreIdeasListComponent {
+export class MoreIdeasListComponent implements OnInit {
+  ideas: MoreIdea[] = [];
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.fetchIdeas();
+  }
+
+  fetchIdeas() {
+    this.http.get<MoreIdea[]>('/api/more-ideas').subscribe({
+      next: (data) => {
+        this.ideas = data;
+      },
+      error: (err) => {
+        console.error('Erro ao buscar ideias:', err);
+      }
+    });
+  }
 }
