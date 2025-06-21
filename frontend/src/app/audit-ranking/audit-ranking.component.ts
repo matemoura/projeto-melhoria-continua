b1de3b410e3194950ae9f99dd251a55232d04666
Mateus Moura <mateus51moura@gmail.com>
2025-06-20 23:31:31 -0300

    feat(frontend): Skeleton of the 5S ranking component

--- frontend/src/app/audit-ranking/audit-ranking.component.ts
+++ frontend/src/app/audit-ranking/audit-ranking.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-audit-ranking',
-  imports: [],
-  templateUrl: './audit-ranking.component.html',
-  styleUrl: './audit-ranking.component.css'
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <h2>Ranking 5S</h2>
+    <p>Ranking de auditorias 5S em construção...</p>
+    `,
+  styles: ``
 })
-export class AuditRankingComponent {
-
-}
+export class AuditRankingComponent { }
