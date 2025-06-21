3d2ddd175ecab29f2c5f30b2d280fa17c44eeae1
Mateus Moura <mateus51moura@gmail.com>
2025-06-21 00:33:45 -0300

    fix(ideas-form): adjust HTML form to include all fields from FormGroup

--- frontend/src/app/more-ideas-form/more-ideas-form.component.ts
+++ frontend/src/app/more-ideas-form/more-ideas-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-more-ideas-form',
