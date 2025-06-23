7f0e9f8084dcd26511bcf060e7e1942f6815b8f8
Mateus Moura <mateus51moura@gmail.com>
2025-06-23 01:08:22 -0300

    refactor: Move HomeComponent styles to dedicated CSS file

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,34 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
-  template: `
-    <h2>Bem-vindo à Plataforma de Melhoria Contínua!</h2>
-    <nav>
-      <ul>
-        <li><a routerLink="/more-ideas/new">Enviar Nova Ideia</a></li>
-        <li><a routerLink="/more-ideas/list">Ver Ideias</a></li>
-        <li><a routerLink="/audit/new">Realizar Auditoria 5S</a></li>
-        <li><a routerLink="/audit/ranking">Ver Ranking 5S</a></li>
-      </ul>
-    </nav>
-  `,
-  styles: `
-    /* Adicione algum estilo básico para sua página inicial */
-    nav ul {
-      list-style-type: none;
-      padding: 0;
-    }
-    nav li {
-      margin: 0.5em 0;
-    }
-    nav a {
-      text-decoration: none;
-      color: #007bff;
-    }
-  `
+  imports: [RouterLink, CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
 })
 export class HomeComponent { }
